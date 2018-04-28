98228a28be8d57de2d360cd3f9b49b93d3937e2d
KentonDunn <kenton.dunn@gmail.com>
2018-04-28 12:39:50 -0400

    updated code to pull zip code and date.  Added an onclick for #submitTwo

---
+++ assets/javascript/options.js
@@ -0,0 +1,38 @@
+console.log("linked");
+
+//creating an on click for submitTwo to record what's been input
+
+$("#submitTwo").on("click", function(event) {
+  console.log("clicked");
+  event.preventDefault();
+
+  //need to set up a function that updates the zip code input from the options page
+
+  var zipCode = $(".zipcode")
+    .val()
+    .trim();
+  console.log(zipCode);
+
+  //I think I need a moment.js function to get the date to appear YYYY-MM-DD
+  var date = $("")
+    .val()
+    .trim();
+  console.log(date);
+});
+
+// Example queryURL for Gracenote API
+var queryURL =
+  "http://data.tmsapi.com/v1.1/movies/showings?startDate=" +
+  date +
+  "&zip=" +
+  zipCode +
+  "&radius=10&api_key=cv2kmy58qkdgyhekzsnz7quz";
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
+
+//with the zipCode variable info get the movie theater information from the GraceNote API
