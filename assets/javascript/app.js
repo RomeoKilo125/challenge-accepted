db4878bc03bb6d74592ddea3941447924e12e129
Solorio <solorio_michelle@yahoo.com>
2018-04-28 11:38:37 -0400

    styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyD9MW7ayHsSrRQ6P50xTLrYKKZY8JWB0Nc",
-    authDomain: "challenge-accepted-405e8.firebaseapp.com",
-    databaseURL: "https://challenge-accepted-405e8.firebaseio.com",
-    projectId: "challenge-accepted-405e8",
-    storageBucket: "",
-    messagingSenderId: "783539858113"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyD9MW7ayHsSrRQ6P50xTLrYKKZY8JWB0Nc",
+  authDomain: "challenge-accepted-405e8.firebaseapp.com",
+  databaseURL: "https://challenge-accepted-405e8.firebaseio.com",
+  projectId: "challenge-accepted-405e8",
+  storageBucket: "",
+  messagingSenderId: "783539858113"
+};
+firebase.initializeApp(config);
